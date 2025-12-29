510084027fc067846347f51ffa1760070d4a0266
Naveed-Iqbal-Devops-Engineer <naveediqbalit@gmail.com>
2025-12-29 06:33:39 +0500

    Change API base URL to localhost for development

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 function App() {
   const [items, setItems] = useState([]);
   const [name, setName] = useState('');
-  const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://192.168.16.5:5001'
+  const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5001'
 
   const fetchItems = async () => {
     const res = await axios.get(`${API_BASE_URL}/api/items`);
